cd8823425c6ab9e76b015d83104c5e5afd14e253
Sam McIngvale <smcingvale@icloud.com>
2014-04-09 00:28:17 -0500

    Add TODO

--- bash/install.rb
+++ bash/install.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+# TODO: If .bash_profile or a symlink already exists,
+# prompt the user to over-write. 
+
 Dir['*.symlink'].each do |file|
   bash_symlink = "#{Dir.home}/.#{file[0, file.index('.')]}"
 

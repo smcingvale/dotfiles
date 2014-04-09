e65bbe1d65c8f2016541807d733b57b3d132ecc0
Sam McIngvale <smcingvale@icloud.com>
2014-04-09 00:25:46 -0500

    Don't create symlink if it already exists

--- bash/install.rb
+++ bash/install.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
 Dir['*.symlink'].each do |file|
-  File.symlink("#{__dir__}/#{file}", "#{Dir.home}/.#{file[0, file.index('.')]}")
+  bash_symlink = "#{Dir.home}/.#{file[0, file.index('.')]}"
+
+  unless File.symlink?(bash_symlink)
+    File.symlink("#{__dir__}/#{file}", "#{Dir.home}/.#{file[0, file.index('.')]}")
+  end
 end

6fd5d29f0602f8656496b12d92b126481eaa6573
Sam McIngvale <smcingvale@icloud.com>
2014-04-10 22:35:00 -0500

    Install vim plugins

--- vim/install.rb
+++ vim/install.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require "fileutils"
 require "open-uri"
+require "yaml"
 
 # TODO: If .vimrc or a symlink (or other vim dotfiles) already exists,
 # prompt the user to over-write.
@@ -18,4 +19,9 @@ FileUtils::mkdir_p(File.join(Dir.home, ".vim", "autoload"))
 
 open(File.join(Dir.home, ".vim", "autoload", "pathogen.vim"), "wb") do |pathogen|
   pathogen << open("https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim").read
+end
+
+vim_plugins = YAML.load_file("plugins.yaml")
+vim_plugins.each do |plugin, clone_url|
+  `git clone git://#{clone_url} #{File.join(Dir.home, ".vim", "bundle", plugin)}`
 end
